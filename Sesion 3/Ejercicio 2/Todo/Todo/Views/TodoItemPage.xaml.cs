6b967c02536e7fc09ef78fa7aea2272db60cbea1
djhvscf <dennishernandezvargas@gmail.com>
2019-01-21 01:02:10 -0600

    Adding Todo example

---
+++ Sesion 3/Ejercicio 2/Todo/Todo/Views/TodoItemPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Todo
+{
+	public partial class TodoItemPage : ContentPage
+	{
+		public TodoItemPage()
+		{
+			InitializeComponent();
+		}
+
+		async void OnSaveClicked(object sender, EventArgs e)
+		{
+			var todoItem = (TodoItem)BindingContext;
+			await App.Database.SaveItemAsync(todoItem);
+			// Utilice Navigation para volver a la lista de items.
+		}
+
+		async void OnDeleteClicked(object sender, EventArgs e)
+		{
+            // Utilice Navigation para volver a la lista de items.
+        }
+
+        async void OnCancelClicked(object sender, EventArgs e)
+		{
+            // Utilice Navigation para volver a la lista de items.
+        }
+    }
+}
---
+++ Sesion 3/Ejercicio 2/Todo/Todo/Views/TodoListPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace Todo
+{
+	public partial class TodoListPage : ContentPage
+	{
+		public TodoListPage()
+		{
+			InitializeComponent();
+		}
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+
+			// Reset the 'resume' id, since we just want to re-start here
+			((App)App.Current).ResumeAtTodoId = -1;
+			listView.ItemsSource = await App.Database.GetItemsAsync();
+		}
+
+		async void OnItemAdded(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new TodoItemPage
+			{
+				BindingContext = new TodoItem()
+			});
+		}
+
+		async void OnListItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+            if (e.SelectedItem != null)
+            {
+                //Agregue aqui la transición a la página de TodoItemPage con el BindingContext del item seleccionado.
+            }
+		}
+	}
+}
