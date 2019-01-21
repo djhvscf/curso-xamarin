9b8f03cb21f3c0c7a7a98eba95d526251d18de68
djhvscf <dennishernandezvargas@gmail.com>
2019-01-21 10:29:49 -0600

    .,

---
+++ Sesion 3/Ejercicio 2/Ejercicio1/Ejercicio1/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Ejercicio2.Localization;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Ejercicio2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+            {
+                var ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+                Resx.AppResources.Culture = ci; // setea el archivo RESX
+                DependencyService.Get<ILocalize>().SetLocale(ci); // setea el hilo para que este al tanto de localization
+            }
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Sesion 3/Ejercicio 2/Ejercicio1/Ejercicio1/Extensions/TranslateExtension.cs
@@ -0,0 +1,44 @@
+﻿using Ejercicio2.Localization;
+using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio2.Extensions
+{
+    // Se puede excluir 'Extension' como sufijo en el XAML
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        readonly CultureInfo ci = null;
+        const string ResourceId = "Ejercicio2.Resx.AppResources";
+
+        static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(
+            () => new ResourceManager(ResourceId, IntrospectionExtensions.GetTypeInfo(typeof(TranslateExtension)).Assembly));
+
+        public string Text { get; set; }
+
+        public TranslateExtension()
+        {
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+            {
+                ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            }
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return string.Empty;
+
+            var translation = ResMgr.Value.GetString(Text, ci);
+            if (translation == null)
+            {
+                translation = Text;
+            }
+            return translation;
+        }
+    }
+}
