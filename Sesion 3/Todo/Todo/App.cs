8ff4c9807eeac7731844eb353a65389addaf9c03
djhvscf <dennishernandezvargas@gmail.com>
2019-01-21 18:08:37 -0600

    ggg

---
+++ Sesion 3/Todo/Todo/App.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Todo
+{
+	public class App : Application
+	{
+		static TodoItemDatabase database;
+
+		public App()
+		{
+			Resources = new ResourceDictionary();
+			Resources.Add("primaryGreen", Color.FromHex("91CA47"));
+			Resources.Add("primaryDarkGreen", Color.FromHex("6FA22E"));
+
+			var nav = new NavigationPage(new TodoListPage());
+			nav.BarBackgroundColor = (Color)App.Current.Resources["primaryGreen"];
+			nav.BarTextColor = Color.White;
+
+			MainPage = nav;
+		}
+
+		public static TodoItemDatabase Database
+		{
+			get
+			{
+				if (database == null)
+				{
+                    database = new TodoItemDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "TodoSQLite.db3"));
+				}
+				return database;
+			}
+		}
+
+		public int ResumeAtTodoId { get; set; }
+	}
+}
+
