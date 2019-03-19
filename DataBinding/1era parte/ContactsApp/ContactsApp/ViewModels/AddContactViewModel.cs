11b0e541cbb3722ac2866d23e19f1c27e5baf4d8
djhvscf <dennishernandezvargas@gmail.com>
2019-03-18 19:35:09 -0600

    ..

---
+++ DataBinding/1era parte/ContactsApp/ContactsApp/ViewModels/AddContactViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ContactsApp.ViewModels
+{
+    public class AddContactViewModel
+    {
+
+        public AddContactViewModel()
+        {
+        }
+
+        string nombre = "Xamarin.Fomrs";
+        string website = "www.google.com";
+        bool esMejorAmigo;
+        bool estaOcupado = false;
+
+        public bool EsMejorAmigo
+        {
+            get { return esMejorAmigo; }
+            set { esMejorAmigo = value; }
+        }
+
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string DisplayMessage
+        {
+            get
+            {
+                return $"Tu nuevo amigo se llama {Nombre} y " +
+                       $"{(esMejorAmigo ? "es" : "no es")} tu mejor amigo.";
+            }
+        }
+
+        public string Website
+        {
+            get
+            {
+                return website;
+            }
+            set { website = value; }
+        }
+
+        public bool EstaOcupado
+        {
+            get { return estaOcupado; }
+            set { estaOcupado = value; }
+        }
+
+        void LaunchWebsite()
+        {
+            try
+            {
+                Device.OpenUri(new Uri(website));
+            }
+            catch
+            {
+
+            }
+        }
+
+        async Task GuardaContacto()
+        {
+            EstaOcupado = true;
+            await Task.Delay(4000);
+
+            EstaOcupado = false;
+
+            await Application.Current.MainPage.DisplayAlert("Guardar", "Contacto ha sido guardado", "OK");
+        }
+
+    }
+}
---
+++ DataBinding/2da parte/ContactsApp/ContactsApp/ViewModels/AddContactViewModel.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ContactsApp.ViewModels
+{
+    public class AddContactViewModel : INotifyPropertyChanged
+    {
+
+        public AddContactViewModel()
+        {
+        }
+
+        string nombre = "Xamarin.Forms";
+        string website = "http://www.google.com";
+        bool esMejorAmigo;
+        bool estaOcupado = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+
+        public bool EsMejorAmigo
+        {
+            get { return esMejorAmigo; }
+            set
+            {
+                esMejorAmigo = value;
+                OnPropertyChanged();
+
+                OnPropertyChanged(nameof(DisplayMessage));
+            }
+        }
+
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set
+            {
+                nombre = value;
+
+                if (nombre == "android")
+                {
+                    estaOcupado = true;
+                }
+                else
+                {
+                    estaOcupado = false;
+                }
+               
+                OnPropertyChanged();
+
+                OnPropertyChanged(nameof(DisplayMessage));
+                OnPropertyChanged(nameof(EstaOcupado));
+            }
+        }
+
+        public string DisplayMessage
+        {
+            get
+            {
+                return $"Tu nuevo amigo se llama {Nombre} y " +
+                     $"{(EsMejorAmigo ? "es" : "no es")} tu mejor amigo.";
+            }
+        }
+
+        public string Website
+        {
+            get
+            {
+                return website;
+            }
+            set
+            {
+                website = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool EstaOcupado
+        {
+            get { return estaOcupado; }
+            set
+            {
+                estaOcupado = value;
+
+                OnPropertyChanged();
+                LaunchWebsiteCommand.ChangeCanExecute();
+                GuardarContactoCommand.ChangeCanExecute();
+            }
+        }
+
+
+        public Command LaunchWebsiteCommand { get; }
+        public Command GuardarContactoCommand { get; }
+
+        void LaunchWebsite()
+        {
+            try
+            {
+                Device.OpenUri(new Uri(website));
+            }
+            catch
+            {
+
+            }
+        }
+
+        async Task GuardaContacto()
+        {
+            EstaOcupado = true;
+            await Task.Delay(4000);
+
+            EstaOcupado = false;
+
+            await Application.Current.MainPage.DisplayAlert("Guardar", "Contacto ha sido guardado", "OK");
+        }
+
+    }
+}
