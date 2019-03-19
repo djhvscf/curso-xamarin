ad6ffd39f3edf35ab0aaf1aaa506584f8b81a736
djhvscf <dennishernandezvargas@gmail.com>
2019-03-18 20:45:13 -0600

    yfrr

---
+++ Listas/Listas/Listas/Lista1.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Listas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Lista1 : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+
+        public Lista1()
+        {
+            InitializeComponent();
+
+            Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5"
+            };
+
+            MyListView.ItemsSource = Items;
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            await DisplayAlert("Item Tapped", "An item was tapped.", "OK");            
+        }
+    }
+}
